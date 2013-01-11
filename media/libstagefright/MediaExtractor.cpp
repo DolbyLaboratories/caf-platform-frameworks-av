08b948d8ba4bbdeb85aa89d3a74ae0d5a9709c9b
Mingming Yin <mingming@codeaurora.org>
2013-01-10 18:42:38 -0800

    auido: Add amr-wb+ codec to ACodec.
    -Add an entry for amr-wb+ decoder in ACodec.
    -amr-wb+ non tunnel will be enabled by default.
    
    Change-Id: Ied8902eb83da29a3164eb99e88630570a43f681e

--- media/libstagefright/MediaExtractor.cpp
+++ media/libstagefright/MediaExtractor.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,13 +104,7 @@ sp<MediaExtractor> MediaExtractor::Create(
         } else {
             ret = new MPEG4Extractor(source);
         }
-        char tunnelDecode[PROPERTY_VALUE_MAX];
-        ALOGV("MediaExtractor::Create checking tunnel.decode");
-        property_get("tunnel.decode",tunnelDecode,"0");
-        if( (strncmp("true",tunnelDecode,4) == 0) || (atoi(tunnelDecode)) ) {
-            bCheckExtendedExtractor = true;
-            ALOGV("MediaExtractor::Create detected tunnel.decode as true...");
-        }
+        bCheckExtendedExtractor = true;
     } else if (!strcasecmp(mime, MEDIA_MIMETYPE_AUDIO_MPEG)) {
         ret = new MP3Extractor(source, meta);
     } else if (!strcasecmp(mime, MEDIA_MIMETYPE_AUDIO_AMR_NB)
