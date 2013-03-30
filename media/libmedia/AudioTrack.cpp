e3c13db97b9d287fec93b3aef6a6ccc68326b935
Mingming Yin <mingming@codeaurora.org>
2013-03-29 22:41:28 -0700

    audio: Add support for tunnel mode recording and compress voip
    - Use modulo operator for common block buffer handling. Common block
      size is rounded to power of two. This limitation is avoided with
      modulo operator.
    - Add support for tunnel mode recording
    - Add support for compress voip
    
    Change-Id: I95cdfff729affd784141487521c9f2f714221d11

--- media/libmedia/AudioTrack.cpp
+++ media/libmedia/AudioTrack.cpp
@@ -1,6 +1,8 @@
 /*
 **
 ** Copyright 2007, The Android Open Source Project
+** Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
+** Not a Contribution.
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -26,6 +28,7 @@
 #include <utils/Log.h>
 #include <private/media/AudioTrackShared.h>
 #include <media/IAudioFlinger.h>
+#include <cutils/properties.h>
 
 #define WAIT_PERIOD_MS                  10
 #define WAIT_STREAM_END_TIMEOUT_SEC     120
@@ -285,6 +288,25 @@ status_t AudioTrack::set(
     mChannelMask = channelMask;
     uint32_t channelCount = popcount(channelMask);
     mChannelCount = channelCount;
+
+    if ((streamType == AUDIO_STREAM_VOICE_CALL) &&
+        (channelCount == 1) &&
+        (sampleRate == 8000 || sampleRate == 16000)) {
+        if (audio_is_linear_pcm(format)) {
+            char propValue[PROPERTY_VALUE_MAX] = {0};
+            property_get("use.voice.path.for.pcm.voip", propValue, "0");
+            bool voipPcmSysPropEnabled = !strncmp("true", propValue, sizeof("true"));
+            if (voipPcmSysPropEnabled) {
+                flags = (audio_output_flags_t)(flags | AUDIO_OUTPUT_FLAG_VOIP_RX |
+                                               AUDIO_OUTPUT_FLAG_DIRECT);
+                ALOGD("Set VoIP and Direct output flags for PCM format");
+            }
+        } else {
+            flags = (audio_output_flags_t)(flags | AUDIO_OUTPUT_FLAG_VOIP_RX |
+                                           AUDIO_OUTPUT_FLAG_DIRECT);
+            ALOGD("Set VoIP and Direct output flags for Non-PCM formats");
+        }
+    }
 
     if (audio_is_linear_pcm(format)) {
         mFrameSize = channelCount * audio_bytes_per_sample(format);
