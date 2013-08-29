c61410db28298409bdf668af8fa6053a87b8c32a
Vikash Garodia <vgarodia@codeaurora.org>
2013-08-29 18:50:37 +0530

    libstagefright: Customize AV sync behavior
    Based on the property, whenever video decoder takes more time
    for decoding and causing few consecutive frame drops to maintain
    the AV sync, then disable AV sync till it recovers.
    
    This change will avoid freeze whenever any video clips out side
    of the supported spec are being played though audio and video
    are out of sync.
    This improves user experience
    
    Change-Id: Ia4ee899ff2515684259b88f66af216871a7bd38c

--- media/libstagefright/include/AwesomePlayer.h
+++ media/libstagefright/include/AwesomePlayer.h
@@ -142,6 +142,8 @@ private:
         TEXTPLAYER_INITIALIZED  = 0x20000,
 
         SLOW_DECODER_HACK   = 0x40000,
+
+        NO_AVSYNC   = 0x80000,
     };
 
     mutable Mutex mLock;
@@ -391,6 +393,7 @@ private:
     AwesomePlayer(const AwesomePlayer &);
     AwesomePlayer &operator=(const AwesomePlayer &);
     bool mReadRetry;
+    bool mCustomAVSync;
 };
 
 }  // namespace android
