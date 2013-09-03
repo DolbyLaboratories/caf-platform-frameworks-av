00b4e75e737f165f9e0265c04e2072c23d47526a
vivek mehta <mvivek@codeaurora.org>
2013-09-03 16:02:41 -0700

    libstagefright: adjust AV sync late margin based on property
    - Current AV late margin is 40 ms
    - Read system property media.sf.set.late.margin and if
      new AV sync late margin is set then use the same
    - Max AV sync late margin allowed is 250 ms
    - Min AV sync late margin is default 40 ms
    
    Change-Id: I65cd3dce9b31b87f1488c37af9618669b29c362c

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -232,6 +232,8 @@ AwesomePlayer::AwesomePlayer()
     mAudioTearDownPosition = 0;
 
     reset();
+
+    mLateAVSyncMargin = ExtendedUtils::ShellProp::getMaxAVSyncLateMargin();
 }
 
 AwesomePlayer::~AwesomePlayer() {
@@ -2026,7 +2028,7 @@ void AwesomePlayer::onVideoEvent() {
             }
         }
 
-        if (latenessUs > 40000) {
+        if (latenessUs > mLateAVSyncMargin) {
             // We're more than 40ms late.
             ALOGV("we're late by %lld us (%.2f secs)",
                  latenessUs, latenessUs / 1E6);
--- media/libstagefright/include/AwesomePlayer.h
+++ media/libstagefright/include/AwesomePlayer.h
@@ -320,6 +320,7 @@ private:
     void printStats();
     int64_t getTimeOfDayUs();
     bool mStatistics;
+    int64_t mLateAVSyncMargin;
 
     struct TrackStat {
         String8 mMIME;
