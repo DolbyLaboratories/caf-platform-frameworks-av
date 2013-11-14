100271204d423715c36ad4c52ffc01fc32bc273b
Fred Oh <fred@codeaurora.org>
2013-11-13 18:42:01 -0800

    frameworks/av: Add audio parameter for sound card status
    Add a parameter to signal sound card status to audio HAL.
    This parameter is set on a change in sound card state.
    
    Change-Id: I9c2265b6197f654f6dab90f7d0b87a3a0921a6fb

--- media/libmedia/AudioParameter.cpp
+++ media/libmedia/AudioParameter.cpp
@@ -41,6 +41,7 @@ const char * const AudioParameter::keyFluence = AUDIO_PARAMETER_KEY_FLUENCE;
 const char * const AudioParameter::keySSR = AUDIO_PARAMETER_KEY_SSR;
 const char * const AudioParameter::keyHandleA2dpDevice = AUDIO_PARAMETER_KEY_HANDLE_A2DP_DEVICE;
 const char * const AudioParameter::keyADSPStatus = AUDIO_PARAMETER_KEY_ADSP_STATUS;
+const char * const AudioParameter::keySoundCardStatus = AUDIO_PARAMETER_KEY_SND_CARD_STATUS;
 const char * const AudioParameter::keyCanOpenProxy = AUDIO_PARAMETER_KEY_CAN_OPEN_PROXY;
 
 AudioParameter::AudioParameter(const String8& keyValuePairs)
