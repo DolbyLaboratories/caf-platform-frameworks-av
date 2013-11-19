679c63713c0d7158421b950577c8f669b3831041
Preetam Singh Ranawat <apranawat@codeaurora.org>
2013-11-20 04:12:55 +0530

    libstagefright: Fix incorrect progress bar display during device switch
     -During offload to nonoffload playback switch (speakr to a2dp)
      audiotear down happens which reset current postion and duration.
     -do not reset the file duration during audio tear down and return
      the cached current position if audioplayer is not created.
    
    Change-Id: I924d270d57aee1a9e79cea1c33bbd6bb731b201d

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -229,6 +229,9 @@ AwesomePlayer::AwesomePlayer()
                               &AwesomePlayer::onAudioTearDownEvent);
     mAudioTearDownEventPending = false;
 
+    mDurationUs = -1;
+    mAudioTearDownPosition = 0;
+
     reset();
 }
 
@@ -615,7 +618,6 @@ void AwesomePlayer::reset_l() {
         shutdownVideoDecoder_l();
     }
 
-    mDurationUs = -1;
     modifyFlags(0, ASSIGN);
     mExtractorFlags = 0;
     mTimeSourceDeltaUs = 0;
@@ -1464,7 +1466,7 @@ status_t AwesomePlayer::getPosition(int64_t *positionUs) {
     } else if (mAudioPlayer != NULL) {
         *positionUs = mAudioPlayer->getMediaTimeUs();
     } else {
-        *positionUs = 0;
+        *positionUs = mAudioTearDownPosition;
     }
     return OK;
 }
