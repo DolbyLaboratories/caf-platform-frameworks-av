b27530423e992b7951bf08ae6d11e7c37059435d
Hui (QRD) Yu <huiyQuic@codeaurora.org>
2014-01-31 22:08:54 +0800

    Add 2 APIs (suspend/resume) in MediaPlayer
    - API:suspend() will just pause the player and release all the decoders
      to replace release() which will release the whole player
    - API:resume() will just init the decoders again,
      then start() will be called to start to play the streaming
    - Add a check in AwesomePlayer::onVideoEvent()
      to make sure the first seek operation will always seek the next i-frame
    
    Change-Id: Ie4c82906a2a056378119921a656128ebdc1007c4

--- media/libstagefright/include/AwesomePlayer.h
+++ media/libstagefright/include/AwesomePlayer.h
@@ -104,6 +104,9 @@ struct AwesomePlayer {
     void postAudioTearDown();
     status_t dump(int fd, const Vector<String16> &args) const;
 
+    status_t suspend();
+    status_t resume();
+
 private:
     friend struct AwesomeEvent;
     friend struct PreviewPlayer;
@@ -204,6 +207,8 @@ private:
 
     bool mWatchForAudioSeekComplete;
     bool mWatchForAudioEOS;
+
+    bool mIsFirstFrameAfterResume;
 
     sp<TimedEventQueue::Event> mVideoEvent;
     bool mVideoEventPending;
