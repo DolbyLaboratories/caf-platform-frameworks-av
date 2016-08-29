21d6f2ac6101e394e54b646443be8107e692550f
Charlie Zheng <czhen@dolby.com>
2016-08-29 15:36:16 +0800

    Merge DAX changes
    Change-Id: I54a6cd8542b7eee7c6a45d7820114bed034ed9e9
    
    Exclude ds1 for DolbyMemoryService
    
    Ds1 should have no DolbyMemoryService.
    
    Change-Id: Ieb17554533a68385db3667a6365e0ea5bec3e4f5

--- media/audioserver/Android.mk
+++ media/audioserver/Android.mk
@@ -1,3 +1,16 @@
+# This file was modified by Dolby Laboratories, Inc. The portions of the
+# code that are surrounded by "DOLBY..." are copyrighted and
+# licensed separately, as follows:
+#
+# (C)  2016 Dolby Laboratories, Inc.
+# All rights reserved.
+#
+# This program is protected under international and U.S. Copyright laws as
+# an unpublished work. This program is confidential and proprietary to the
+# copyright owners. Reproduction or disclosure, in whole or in part, or the
+# production of derivative works therefrom without the express permission of
+# the copyright owners is prohibited.
+#
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -17,6 +30,11 @@ LOCAL_SHARED_LIBRARIES := \
 	libradioservice \
 	libsoundtriggerservice \
 	libutils
+
+ifdef DOLBY_ENABLE
+LOCAL_SHARED_LIBRARIES += $(dolby_shared_service_lib)
+LOCAL_STATIC_LIBRARIES += $(dolby_static_service_lib)
+endif
 
 LOCAL_C_INCLUDES := \
 	frameworks/av/services/audioflinger \
